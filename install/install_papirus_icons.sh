b116721dc74573df48ab4afc1f292aa5fc91d674
shvedes <shved.personal@gmail.com>
2025-10-19 19:57:47 +0200

    fix: wrong link to papirus-folders script

--- install/install_papirus_icons.sh
+++ install/install_papirus_icons.sh
@@ -3,8 +3,8 @@
 set -euo pipefail
 
 YELLOW=$'\e[0;33m'
-GREEN=$'\e[0;32m'
 RESET=$'\e[0m'
+LINK="https://raw.githubusercontent.com/PapirusDevelopmentTeam/papirus-folders/refs/heads/master/papirus-folders"
 
 # Due to the active use of the papirus-folders script, 
 # we need to keep the Papirus icon pack accessible for writing by regular users in order to avoid constant use of sudo.
@@ -12,15 +12,12 @@ RESET=$'\e[0m'
 CWD="$(pwd)"
 
 printf "\r\033[K%s[VIBRANIUM]%s Installing icon theme" "${YELLOW}" "${RESET}"
-cd ~/.cache
-git clone -q https://github.com/PapirusDevelopmentTeam/papirus-icon-theme
-cd papirus-icon-theme
-mkdir -p "$HOME/.local/share/icons"
+cd ~/.cache; git clone -q https://github.com/PapirusDevelopmentTeam/papirus-icon-theme
+cd papirus-icon-theme; mkdir -p "$HOME/.local/share/icons"
 cp -r Papirus* "$HOME"/.local/share/icons
 
 mkdir -p "$HOME/.local/bin"
-curl -s https://raw.githubusercontent.com/PapirusDevelopmentTeam/papirus-folders/refs/heads/master/install.sh \
-	-o "$HOME/.local/bin/papirus-folders"
+curl -s "$LINK"	-o "$HOME/.local/bin/papirus-folders"
 chmod +x "$HOME/.local/bin/papirus-folders"
 printf "\r\033[K%s[VIBRANIUM]%s Icon theme installed" "${YELLOW}" "${RESET}"
 sleep 0.5
