c01b2a6141ba7ba61b5794117df443c0aa11b9a6
shvedes <shved.personal@gmail.com>
2025-10-25 14:32:05 +0200

    am i actually retarded

--- install/generate_migrations.sh
+++ install/generate_migrations.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+VIBRANIUM="$HOME/.local/share/vibranium"
 STATE_DIR="$HOME/.local/state/vibranium/migrations"
+MIGRATIONS_DIR="$VIBRANIUM/migrations"
+
 mkdir -p "$STATE_DIR"
 
-for file in ./migrations/*.sh; do
-	touch "$STATE_DIR/$file"
+for file in "$MIGRATIONS_DIR"/*.sh; do
+    touch "$STATE_DIR/$(basename "$file")"
 done
