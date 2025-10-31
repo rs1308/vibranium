8178bc10a942d2c80af2249c81816609c45cac58
shvedes <shved.personal@gmail.com>
2025-10-31 16:51:42 +0100

    chore(install): use 5 minutes until suspend for laptops

--- install/generate_hypridle_config.sh
+++ install/generate_hypridle_config.sh
@@ -3,7 +3,7 @@
 case "$(</sys/class/dmi/id/chassis_type)" in
     9|10|14)  # Laptop, Notebook, Sub-Notebook
         LOCK_AFTER=120
-		SLEEP_AFTER=600
+		SLEEP_AFTER=300
         ;;
     *)  # Desktop / Other
         LOCK_AFTER=600
